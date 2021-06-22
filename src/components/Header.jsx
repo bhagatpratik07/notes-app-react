0990afa50d82fecd49163e5eed8df034b1126f9a
Pratik Bhagat <bhagatpratik.2002@gmail.com>
2021-06-22 16:43:54 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { MdBrush } from "react-icons/md";
 
-export default function Header({ toggleDarkMode }) {
+export default function Header({ setDarkTheme }) {
   return (
     <div className="header">
-      <h1>Notes</h1>
-      <button onClick={() => toggleDarkMode((prev) => !prev)} className="theme">
-        Change Theme
-      </button>
+      <h1>My Notes</h1>
+
+      <MdBrush
+        className="theme-btn"
+        size="1.5rem"
+        onClick={() => setDarkTheme((prev) => !prev)}
+      />
     </div>
   );
 }
+
